ba1ecceed70c9474da63e2b7c80a89f0c76d3dfc
Marcos Oliveira <marcosoliveira38@hotmail.com>
2024-03-10 22:10:15 -0300

    Tela de Login

---
+++ script.js
@@ -0,0 +1,6 @@
+var input = document.querySelector('#input input');
+var img = document.querySelector('#input img');
+img.addEventListener('click', function () {
+  input.type = input.type == 'text' ? 'password' : 'text';
+});
+
