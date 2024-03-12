1e56da4eb0361951fe7606995d3748c660554709
marcosoliveira_dev <marcosoliveira253.mo@gmail.com>
2024-03-12 10:16:33 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var input = document.querySelector('#input input');
-var img = document.querySelector('#input img');
-img.addEventListener('click', function () {
-  input.type = input.type == 'text' ? 'password' : 'text';
-});
-
