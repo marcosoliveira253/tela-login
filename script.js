c50980eb6d5ecf215261c834fc3931234396f778
marcosoliveira_dev <marcosoliveira253.mo@gmail.com>
2024-03-12 11:30:16 -0300

    Atualização do código javascript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var input = document.querySelector('#input input');
 var img = document.querySelector('#input img');
+
 img.addEventListener('click', function () {
     input.type = input.type == 'text' ? 'password' : 'text';
 });
+
+function mudarImagem(img) {
+    let input = document.getElementById('password');
+    if (input.type === "password") {
+        input.type == "text";
+        img.src = "./img/olho.png";
+    } else {
+        input.type == "password";
+        img.src = "./img/olho-fechado.png";
+    }
+}
